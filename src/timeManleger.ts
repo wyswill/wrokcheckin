20a3c49155432ab560d0b4e0eebd0995c7a61cc7
wyswill <1151373571@qq.com>
2020-05-13 12:56:40 +0800

    修复保存数据时json格式损坏的问题

--- src/timeManleger.ts
+++ src/timeManleger.ts
@@ -1,23 +1,20 @@
 /// <reference path="./type.d.ts" />
-const fs = require('fs'), os = require('os');
+const fs = require("fs"),
+  os = require("os");
 const homepath = os.homedir();
-const dbPath = `${homepath}/db.json`, configPath = `${homepath}/config.json`;
-const chalk = require('chalk');
-const symbols = require('log-symbols');
+const dbPath = `${homepath}/db.json`,
+  configPath = `${homepath}/config.json`;
+const chalk = require("chalk");
+const symbols = require("log-symbols");
 export default class timeManleger {
   public db: STORE = {};
-  public config: answrod = {lh: 9, lm: 30};
+  public config: answrod = { lh: 9, lm: 30 };
 
   /**
    * 保存数据到缓存中
    */
   save() {
-    const writeStream = fs.createWriteStream(dbPath, {flags: 'r+'});
-    writeStream.write(JSON.stringify(this.db));
-    writeStream.on('drain', () => {
-      console.log(symbols.error, chalk.red('内存不够用了!清除内存!'));
-    })
-    writeStream.end();
+    this.write(dbPath, JSON.stringify(this.db));
   }
 
   /**
@@ -25,24 +22,26 @@ export default class timeManleger {
    */
   saveConfig(answers: answrod) {
     this.config = answers;
-    const writeStream = fs.createWriteStream(configPath, {flags: 'r+'},);
-    writeStream.write(JSON.stringify(this.config));
-    writeStream.on('drain', () => {
-      console.log(symbols.error, chalk.red('内存不够用了!清除内存!'));
-    })
+    this.write(configPath, JSON.stringify(this.config));
+  }
+  private write(path: string, data: string | Buffer) {
+    const writeStream = fs.createWriteStream(path);
+    writeStream.write(data);
+    writeStream.on("drain", () => {
+      console.log(symbols.error, chalk.red("内存不够用了!清除内存!"));
+    });
     writeStream.end();
   }
-
   /**
    * 加载本地缓存
    */
   lodeData() {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
       fs.exists(dbPath, (exists: boolean) => {
         if (exists) {
           const readStream = fs.createReadStream(dbPath);
-          readStream.setEncoding('UTF8');
-          let data = '';
+          readStream.setEncoding("UTF8");
+          let data = "";
           readStream.on("data", (chunk: string) => {
             data += chunk;
           });
@@ -51,14 +50,14 @@ export default class timeManleger {
             resolve();
           });
         } else {
-          fs.writeFile(dbPath, '', (err: Error) => {
+          fs.writeFile(dbPath, "", (err: Error) => {
             if (err) throw err;
           });
           this.db = {};
           resolve();
         }
       });
-    })
+    });
   }
 
   /**
@@ -68,19 +67,17 @@ export default class timeManleger {
     fs.exists(configPath, (exists: boolean) => {
       if (exists) {
         const fileReader = fs.createReadStream(configPath);
-        fileReader.setEncoding('UTF8');
-        let data = '';
-        fileReader.on("data", (chunk: string) => data += chunk);
+        fileReader.setEncoding("UTF8");
+        let data = "";
+        fileReader.on("data", (chunk: string) => (data += chunk));
         fileReader.on("end", () => {
-          if (data != '')
-            this.config = JSON.parse(data);
+          if (data != "") this.config = JSON.parse(data);
         });
-
       } else {
-        fs.writeFile(configPath, '', (err: Error) => {
+        fs.writeFile(configPath, "", (err: Error) => {
           if (err) throw err;
         });
-        this.config = {lh: 9, lm: 30};
+        this.config = { lh: 9, lm: 30 };
       }
     });
     return this;
